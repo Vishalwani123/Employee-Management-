e87e5a10f38febc99b73cbbfac60bcf7e3e059fe
Vishalwani123 <93905384+Vishalwani123@users.noreply.github.com>
2024-12-25 14:41:33 +0530

    Employee Management

---
+++ EmployeeManagement/EmployeeWage.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement
+{
+    internal class EmployeeWage
+    {
+        public const int WagePerHour = 20;
+        public const int FullDayHours = 8;
+        public const int PartTimeHours = 8;
+        public const int WorkingDaysPerMonth= 20;
+        public const int MaxWorkingHours = 100;
+
+        private Random random = new Random();
+
+        public bool IsEmployeePresent()
+        {
+            return random.Next(0, 2) == 1;
+        }
+        public int CalculateDailyWage(int workingHours)
+        {
+            return WagePerHour * workingHours;
+        }
+        public int CalculateMonthlyWage()
+        {
+            int totalWage = 0;
+
+            for (int day = 1; day <= WorkingDaysPerMonth; day++)
+            {
+                int workingHours = GetWorkingHoursForDay();
+                totalWage += CalculateDailyWage(workingHours);
+            }
+
+            return totalWage;
+        }
+        public int CalculateWageWithConditions()
+        {
+            int totalWage = 0, totalHours = 0, totalDays = 0;
+
+            while (totalHours < MaxWorkingHours && totalDays < WorkingDaysPerMonth)
+            {
+                totalDays++;
+                int workingHours = GetWorkingHoursForDay();
+                totalHours += workingHours;
+                totalWage += CalculateDailyWage(workingHours);
+            }
+
+            return totalWage;
+        }
+        private int GetWorkingHoursForDay()
+        {
+            int attendance = random.Next(0, 3);
+
+            switch (attendance)
+            {
+                case 1: return FullDayHours; 
+                case 2: return PartTimeHours; 
+                default: return 0; 
+            }
+        }
+    }
+}
---
+++ EmployeeManagement/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EmployeeWage ew = new EmployeeWage();
+
+            Console.WriteLine();
+            Console.WriteLine("Employee Wage System : ");
+
+            Console.WriteLine();
+            Console.WriteLine(" Checking Employee Attendance...");
+            bool isPresent = ew.IsEmployeePresent();
+            Console.WriteLine(isPresent ? "Employee is Present" : "Employee is Absent");
+
+            if (isPresent)
+            {
+                Console.WriteLine("Calculating Daily Wage...");
+                int dailyWage = ew.CalculateDailyWage(EmployeeWage.FullDayHours);
+                Console.WriteLine($"Daily Wage: {dailyWage}");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Adding Part-Time Employee & Calculating Daily Wage...");
+            int partTimeWage = ew.CalculateDailyWage(EmployeeWage.PartTimeHours);
+            Console.WriteLine($"Part-Time Daily Wage: {partTimeWage}");
+
+            Console.WriteLine();
+            Console.WriteLine("Calculating Monthly Wage...");
+            int monthlyWage = ew.CalculateMonthlyWage();
+            Console.WriteLine($"Monthly Wage (Full-Time): {monthlyWage}");
+
+            Console.WriteLine();
+            Console.WriteLine("Calculating Wages Till Condition of Total Hours/Days...");
+            int conditionalWage = ew.CalculateWageWithConditions();
+            Console.WriteLine($"Wages Till Condition Met: {conditionalWage}");
+
+        }
+    }
+}
